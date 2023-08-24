0a088690734ff8235bf8578e2dbfd56e6fd23512
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-24 20:54:02 +0100

    feat: use note model

--- app/Controllers/Http/NotesController.ts
+++ app/Controllers/Http/NotesController.ts
@@ -1,14 +1,32 @@
+import { schema } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
+import Note from 'App/Models/Note'
 
 export default class NotesController {
-    public async index({ response }: HttpContextContract) {
-        const notes = await Database.from('notes').select('*')
+    public async index({ response, auth }: HttpContextContract) {
+        await auth.use('api').authenticate()
+
+        const notes = await Database.from('notes')
+            .select('*')
+            .where('user', auth.use('api').user?.id!)
         response.json({ data: notes })
     }
 
-    public async store({ request, response }: HttpContextContract) {
-        const { message } = request.body()
-        response.json({ message })
+    public async store({ request, response, auth }: HttpContextContract) {
+        await auth.use('api').authenticate()
+
+        const noteSchema = schema.create({
+            message: schema.string({ trim: true }),
+        })
+
+        const data = await request.validate({ schema: noteSchema })
+
+        const note = await Note.create({
+            message: data.message,
+            userId: auth.use('api').user!.id,
+        })
+
+        response.json({ data: { note } })
     }
 }
