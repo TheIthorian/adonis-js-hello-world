b4d01b9e23adf3382de9a1a5639f33d397101b59
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-25 19:37:17 +0100

    feat: add notes input

--- ui/src/components/notes/index.tsx
+++ ui/src/components/notes/index.tsx
@@ -1,6 +1,6 @@
 import { CONFIG } from '@/config'
 import React from 'react'
-import { Box, Card, Typography } from '@mui/material'
+import { Box, Button, Card, Typography, Input } from '@mui/material'
 
 async function getNotes() {
     const res = await fetch(CONFIG.API_URL + '/notes', {
@@ -13,11 +13,11 @@ async function getNotes() {
         throw new Error('Error fetching data', { cause: await res.json() })
     }
 
-    return await res.json()
+    return (await res.json()).data
 }
 
 export function Notes() {
-    const [notes, setNotes] = React.useState<{ data: Array<{ id: number; message: string }> }>()
+    const [notes, setNotes] = React.useState<Array<{ id: number; message: string }>>()
 
     React.useEffect(() => {
         getNotes()
@@ -32,17 +32,71 @@ export function Notes() {
     }
 
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-            }}
-        >
-            {notes.data.map((note) => (
-                <Card key={note.id} sx={{ m: 1, p: 1 }}>
-                    <Typography>{note.message}</Typography>
-                </Card>
-            ))}
-        </Box>
+        <>
+            <Card
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    m: 1,
+                    p: 1,
+                }}
+            >
+                <AddNote
+                    onAdd={(id, message) => {
+                        setNotes((oldNotes) => [{ id, message }, ...(oldNotes ?? [])])
+                    }}
+                />
+            </Card>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}
+            >
+                {notes.map((note) => (
+                    <Card key={note.id} sx={{ m: 1, p: 1 }}>
+                        <Typography>{note.message}</Typography>
+                    </Card>
+                ))}
+            </Box>
+        </>
+    )
+}
+
+function AddNote({ onAdd }: { onAdd: (id: number, message: string) => void }) {
+    const [message, setMessage] = React.useState('')
+    const [error, setError] = React.useState('')
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const res = await fetch(CONFIG.API_URL + '/notes', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('token') ?? '',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ message }),
+        })
+
+        const responseData = await res.json()
+        console.log({ responseData })
+        onAdd(responseData.data.note.id, responseData.data.note.message)
+        setMessage('')
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <Input
+                type="text"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Add a note"
+                sx={{ width: '100%' }}
+            />
+            <Button sx={{ mt: 2 }} type="submit" disabled={error !== '' || !message}>
+                Add
+            </Button>
+            {error && <p>{error}</p>}
+        </form>
     )
 }
