9ec1713a24b89274259ce094294d6d713640473f
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-25 19:44:41 +0100

    feat: handle logout

--- ui/src/components/nav.tsx
+++ ui/src/components/nav.tsx
@@ -1,27 +1,11 @@
 import * as React from 'react'
 import AccountCircle from '@mui/icons-material/AccountCircle'
-import MenuIcon from '@mui/icons-material/Menu'
 
-import {
-    AppBar,
-    Box,
-    Button,
-    Divider,
-    Drawer,
-    IconButton,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
-    Menu,
-    MenuItem,
-    Toolbar,
-    Typography,
-} from '@mui/material'
+import { AppBar, Box, Button, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material'
 import { Dashboard, QuestionMark, Upload } from '@mui/icons-material'
 import Link from 'next/link'
 import { CONFIG } from '@/config'
+import { useRouter } from 'next/router'
 
 const i18n = {
     login: 'Login',
@@ -30,10 +14,7 @@ const i18n = {
 
 export function Nav() {
     const [auth, setAuth] = React.useState(false)
-
-    const handleLogin = (authData: boolean) => {
-        setAuth(authData)
-    }
+    const router = useRouter()
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -42,20 +23,14 @@ export function Nav() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1, marginLeft: 1 }}>
                         <Link href="/">{i18n.appTitle}</Link>
                     </Typography>
-                    <UserProfileButton auth={auth} handleLogin={handleLogin} />
+                    <UserProfileButton auth={auth} onLogout={() => router.push('/login')} />
                 </Toolbar>
             </AppBar>
         </Box>
     )
 }
 
-function UserProfileButton({
-    auth,
-    handleLogin,
-}: {
-    auth: boolean
-    handleLogin: (authData: boolean) => void
-}) {
+function UserProfileButton({ auth, onLogout }: { auth: boolean; onLogout: () => void }) {
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
 
     const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -63,24 +38,17 @@ function UserProfileButton({
     }
 
     const handleLogout = async () => {
-        handleLogin(false)
         await fetch(CONFIG.AUTH_BASE + '/logout', {
             method: 'POST',
-            headers: { jwt: localStorage.getItem('jwt') ?? '' },
+            headers: { Authorization: 'Bearer ' + localStorage.getItem('token') ?? '' },
         })
+        localStorage.removeItem('token')
         handleClose()
+        onLogout()
     }
 
     const handleClose = () => {
         setAnchorEl(null)
-    }
-
-    if (!auth) {
-        return (
-            <Button color="inherit" onClick={() => handleLogin(true)}>
-                Login
-            </Button>
-        )
     }
 
     return (
@@ -110,7 +78,6 @@ function UserProfileButton({
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
             >
-                <MenuItem onClick={handleClose}>My account</MenuItem>
                 <MenuItem onClick={() => handleLogout()}>Log out</MenuItem>
             </Menu>
         </div>
